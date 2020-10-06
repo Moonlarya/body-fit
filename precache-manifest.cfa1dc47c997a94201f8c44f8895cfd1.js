dec99a3cf5cbb97b2fdc45f443511527026fc985
Moonlarya <gum225@gmail.com>
2020-10-07 01:05:18 +0300

    Updates

---
+++ precache-manifest.cfa1dc47c997a94201f8c44f8895cfd1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a1e763cdefc8f2e7619ecf5246cd837",
+    "url": "/body-fit/index.html"
+  },
+  {
+    "revision": "6636fa0827fc4435ead0",
+    "url": "/body-fit/static/css/main.cdd17b0d.chunk.css"
+  },
+  {
+    "revision": "8bb3fd71d648334689e7",
+    "url": "/body-fit/static/js/2.1e0ce824.chunk.js"
+  },
+  {
+    "revision": "1599112f5867042ef505091e0fb03a4e",
+    "url": "/body-fit/static/js/2.1e0ce824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6636fa0827fc4435ead0",
+    "url": "/body-fit/static/js/main.a8c65516.chunk.js"
+  },
+  {
+    "revision": "96e6d27e0f0873b10e94",
+    "url": "/body-fit/static/js/runtime-main.402f82b2.js"
+  },
+  {
+    "revision": "7aee99049c50ec14ae4b586cffb60328",
+    "url": "/body-fit/static/media/food.7aee9904.svg"
+  },
+  {
+    "revision": "b397db7cd991a1731400ab67306f3ebf",
+    "url": "/body-fit/static/media/home.b397db7c.svg"
+  },
+  {
+    "revision": "110e1229df3c26ec1c89fa52c13ac86c",
+    "url": "/body-fit/static/media/tracker.110e1229.svg"
+  },
+  {
+    "revision": "83d96e860d923631429999ac3d7ff4ad",
+    "url": "/body-fit/static/media/workout.83d96e86.svg"
+  }
+]);
