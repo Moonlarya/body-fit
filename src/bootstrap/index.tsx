58b89a09336aacfbdbe7860563bcfc97bbe61cac
Moonlarya <gum225@gmail.com>
2020-10-07 16:13:12 +0300

    [fix] try to fix router

--- src/bootstrap/index.tsx
+++ src/bootstrap/index.tsx
@@ -1,6 +1,6 @@
 import Footer from "components/Footer";
 import React, { FC } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
