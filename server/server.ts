a20fc66ecf88d9289cde1dbf5202c85d63275ce2
Moonlarya <gum225@gmail.com>
2020-10-08 16:26:52 +0300

    [add] removed favicon

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,9 @@
 const path = require("path");
 const express = require("express");
-const favicon = require("express-favicon");
 
 const port = process.env.PORT || 8080;
 
 const app = express();
-
-// eslint-disable-next-line
-app.use(favicon(__dirname + "/build/favicon.png"));
 
 app.use(express.static(__dirname));
 
