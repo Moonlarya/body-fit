75903beb17b11c4f06909891e7d92d3e63a53c4f
Moonlarya <gum225@gmail.com>
2020-10-08 17:31:31 +0300

    [fix] start command

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 
 // eslint-disable-next-line
-app.get("*", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
