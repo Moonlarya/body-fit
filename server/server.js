ebe0459d51f66c743640df5408d217c3ed1d3e90
Moonlarya <gum225@gmail.com>
2020-10-08 17:40:05 +0300

    [fix] start command

--- server/server.js
+++ server/server.js
@@ -5,14 +5,12 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(__dirname));
-
 // eslint-disable-next-line
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 // eslint-disable-next-line
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 app.listen(port, () => {
