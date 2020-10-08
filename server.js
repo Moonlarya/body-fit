1e9459ddb75c79117e7ba818666370bf00185752
Moonlarya <gum225@gmail.com>
2020-10-08 14:24:50 +0300

    [add] server for deploying

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import favicon from "express-favicon";
+import * as path from "path";
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+// eslint-disable-next-line
+app.use(favicon(__dirname + "/build/favicon.png"));
+
+app.use(express.static(__dirname));
+
+// eslint-disable-next-line
+app.use(express.static(path.join(__dirname, "build")));
+
+// eslint-disable-next-line
+app.get("/ping", function (res) {
+  return res.send("pong");
+});
+
+// eslint-disable-next-line
+app.get("/*", function (res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);
